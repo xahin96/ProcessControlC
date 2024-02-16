22a720dda5377cdbe202fee2dc2dabd0be682d23
zahin <xahin96@gmail.com>
2024-02-16 15:52:02 -0500

    NULL pointer test

--- chdirfork.c
+++ chdirfork.c
@@ -3,34 +3,29 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main()
-{
-    int p,p1;
+int main() {
+    int p, p1;
     p = fork();
-    
-    if(p==-1)
-    {
+
+    if (p == -1) {
         printf("There is an error while calling fork()");
     }
-    if(p==0)
-    {   
+    if (p == 0) {
         printf("\nWe are in the child process\n");
-       char *s;
-    // current working directory
-        printf("\nThe current working directory of the child process is %s\n",getcwd(s,50));
-    // using the system call chdir()
-    chdir("/home/pranga/Documents");
-    
-    printf("\nThe new working directory of the child process is %s\n",getcwd(s,50));
-    // new working directory
-    
+        char s[100];
+        // current working directory
+        printf("\nThe current working directory of the child process is %s\n", getcwd(s, 100));
+        // using the system call chdir()
+        chdir("/Users/zahin/My Files/");
+
+        printf("\nThe new working directory of the child process is %s\n", getcwd(s, 100));
+        // new working directory
+
         exit(0);
-    }
-    else
-    {
-      int k=wait();
+    } else {
+        int k = wait(0);
         printf("\nParent process resumes\n");
-        char  *s;
+        char *s;
         exit(0);
     }
     return 0;
--- forkexec.c
+++ forkexec.c
@@ -2,26 +2,46 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 int main() {
-    int p;
-    p = fork();
+    int p = fork();
 
     if (p == -1) {
-        printf("There is an error while calling fork()");
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
     if (p == 0) {
         printf("\nWe are in the child process\n");
         printf("\nThe child process is now being replaced by another executable using execl");
-        int i = execl("/bin/ls", "bin/ls", "-1", NULL);
-//        int i = execl("xyz100", "xyz100", "-1", NULL);
-
+        int i = execl("/Users/zahin/My Files/uWindsor/Sem 2/COMP 8567/Assignment 2/Code2/fileutil",
+                      "fileutil",
+                      "/Users/zahin/My Files/test/task 1 copy 30",
+                      "a1.c",
+                      NULL);
+//        int i = execlp("/Users/zahin/My Files/uWindsor/Sem 2/COMP 8567/Assignment 2/Code2/fileutil",
+//                       "fileutil",
+//                       "/Users/zahin/My Files/test/task 1 copy 30/folder2",
+//                       "/Users/zahin/My Files/test/task 1 copy 30/folder1",
+//                       "-cp",
+//                       "a1.c",
+//                       NULL);
+        if (i == -1) {
+            perror("execl");
+            exit(EXIT_FAILURE);
+        }
         printf("\nWe are in the child process\n");
-        exit(0);
+        exit(EXIT_SUCCESS);
     } else {
-        int k = wait(0);
+        int status;
+        int k = wait(&status);
+        if (k == -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
         printf("\nWe are in the parent process\n");
-        exit(0);
+        exit(EXIT_SUCCESS);
     }
     return 0;
 }
