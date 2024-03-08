883796c7f05828d0b79bcdc5aaae6b2a848d6c5c
zahin <xahin96@gmail.com>
2024-03-08 17:39:53 -0500

    formatted

--- op.c
+++ op.c
@@ -2,36 +2,29 @@
 #include <unistd.h>
 
 
-   int main(void)
-	{
-	 int i=fork();
+int main(void) {
+    int i = fork();
 
-		
-	if(i==0)
-	{
-	for(;;)
-	{
-	sleep(1);
-	printf("\n\nCHILD PROCESS\n");
-	printf("\n The id of the child process is %d \n",getpid());	
-	printf("\n The parent id of the child process is %d \n",getppid());
-	}
-	}
-	else if(i<0)
-	{
-	printf("\n\nERROR\n");
 
-	}
-	else{
-	for(;;)
-	{
-	sleep(1);
-	printf("\n\nPARENT PROCESS\n");
-	printf("\n The id of the parent process is %d \n",getpid());	
-	printf("\n The parent id of the parent process is %d \n",getppid());
-	} 
-	}
+    if (i == 0) {
+        for (;;) {
+            sleep(1);
+            printf("\n\nCHILD PROCESS\n");
+            printf("\n The id of the child process is %d \n", getpid());
+            printf("\n The parent id of the child process is %d \n", getppid());
+        }
+    } else if (i < 0) {
+        printf("\n\nERROR\n");
+
+    } else {
+        for (;;) {
+            sleep(1);
+            printf("\n\nPARENT PROCESS\n");
+            printf("\n The id of the parent process is %d \n", getpid());
+            printf("\n The parent id of the parent process is %d \n", getppid());
+        }
     }
+}
 
 
 
